f9b78bc4614d0a385785633c0e4be827dc0550ce
ccding_ustc <andxdcc@sina.com>
2016-09-28 10:41:18 +0800

    add DeleteDuplicates.java

---
+++ src/DeleteDuplicates.java
@@ -0,0 +1,33 @@
+
+/**
+ * 
+ * @ClassName: DeleteDuplicates
+ *
+ * @Description: TODO Given a sorted linked list, delete all duplicates 
+ * such that each element appear only once.
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+ *
+ * @author: ccding
+ * @date: 2016年9月28日 上午9:46:12
+ *
+ */
+public class DeleteDuplicates {
+	public static ListNode deleteDuplicates(ListNode head){
+		if(head == null || head.next == null)
+			return head;
+		ListNode p = head;
+		ListNode r = p.next;
+		while(r != null){
+			if(p.val == r.val){
+				r = r.next;
+				p.next = r;
+			}else{
+				p = r;
+				r = r.next;
+			}
+		}
+		return head;
+	}
+}
