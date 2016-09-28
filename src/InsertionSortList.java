24bc1911feea1e8db0f097c0e7866e7efc86295c
ccding_ustc <andxdcc@sina.com>
2016-09-28 08:54:08 +0800

    modified InsetionSortList.java

--- src/InsertionSortList.java
+++ src/InsertionSortList.java
@@ -1,16 +1,23 @@
 
 public class InsertionSortList {
 	public ListNode insertionSortList(ListNode head) {
-		ListNode l = head;
-		ListNode node = head;
-		while(node != null){
-			while(node.val > l.val)
-				l = l.next;
-			node.next = l.next;
-			l.next = node;
-			node = node.next;
-			
+		if(head == null || head.next == null)
+			return head;
+		ListNode headDummy = new ListNode(0);
+		while(head != null){
+			ListNode next = head.next;
+			insert(headDummy, head);
+			head = next;
 		}
+		return headDummy.next;		
 	}
-
+	
+	
+	private void insert(ListNode listDummy, ListNode target){
+		ListNode curr = listDummy;
+		while(curr.next != null && curr.next.val < target.val)
+			curr = curr.next;
+		target.next = curr.next;
+		curr.next = target;
+	}
 }
