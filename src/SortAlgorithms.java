963a21399f6a4d206fa9777186bdf067e3cd64d2
ccding_ustc <andxdcc@sina.com>
2017-02-13 21:17:44 +0800

    modified

--- src/SortAlgorithms.java
+++ src/SortAlgorithms.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 
  * @ClassName: SortAlgorithms
@@ -5,6 +7,8 @@
  * @Description: 常见的排序算法 java 实现
  * void bubbleSort(T[]) 冒泡排序
  * void insertSort(T[]) 插入排序
+ * void selectSort(T[]) 选择排序
+ * void shellSort(T[]) 希尔排序
  * @author: ccding
  * @date: 2016年10月26日 上午10:50:55
  *
@@ -37,5 +41,72 @@ public class SortAlgorithms <T extends Comparable<T>> {
 		}
 	}
 	
+	public void selectSort(T[] nums){
+		T tmp;
+		int minIndex;
+		for(int i=0; i<nums.length; i++){
+			minIndex = this.getMinIndex(nums, i);
+			tmp = nums[minIndex];
+			nums[minIndex] = nums[i];
+			nums[i] = tmp;
+		}
+			
+		
+	}
 	
+	int getMinIndex(T[] nums, int startIndex){
+		T minValue = nums[startIndex];
+		int minIndex = startIndex;
+		for(int i=startIndex; i<nums.length; i++){
+			if(minValue.compareTo(nums[i]) > 0){
+				minValue = nums[i];
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
+	
+	public void shellSort(T[] nums){
+		int gap = nums.length >> 1;
+		T tmp;
+		while(gap >= 1){
+			for(int i=gap; i<nums.length; i++){
+				if(nums[i].compareTo(nums[i-gap]) < 0){
+					tmp = nums[i];
+					int j = i-gap;
+					while( j>=0 && tmp.compareTo(nums[j])<0){
+						nums[j+gap] = nums[j];
+						j -= gap;
+					}
+					nums[j+gap] = tmp;
+				}
+			}
+			gap = gap >> 1;
+		}
+	}
+	public void mergeSort(T[] nums){
+		mergeSort(nums, 0, nums.length);
+	}
+	private void mergeSort(T[] nums, int startIndex, int endIndex){
+		if(endIndex > startIndex){
+			int midIndex = (endIndex-startIndex) >> 1;
+			mergeSort(nums, startIndex, midIndex);
+			mergeSort(nums, midIndex+1, endIndex);
+			merge(nums, startIndex, midIndex, endIndex);
+		}
+	}
+	private void merge(T[] nums, int s, int m, int e){
+		T[] sub1 = Arrays.copyOfRange(nums, s, m);
+		T[] sub2 = Arrays.copyOfRange(nums, m, e);
+		int idx1 = 0, idx2 = 0;
+		while(idx1<sub1.length && idx2<sub2.length){
+			if(sub1[idx1].compareTo(sub2[idx2]) < 0){
+				nums[idx1+idx2] = sub1[idx1];
+				idx1++;
+			}else{
+				nums[idx1+idx2] = sub2[idx2];
+				idx2++;
+			}
+		}
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,9 @@
 
 public class Test{
 	public static void main(String[] args) {
-		Integer[] nums = {1,2,6,43,2,12};
+		Integer[] nums = {1,0,0,2,6,4,3,2,12,13};
 		SortAlgorithms<Integer> sa = new SortAlgorithms<>();
-		sa.bubbleSort(nums);
+		sa.mergeSort(nums);
 		for(Integer i: nums)
 			System.out.print(i + " ");
 		
