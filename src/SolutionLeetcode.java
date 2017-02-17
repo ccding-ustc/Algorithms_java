f858627d7e57041a7196f8745923dbf7c240e5b5
ccding_ustc <andxdcc@sina.com>
2017-02-17 15:50:31 +0800

    modified

--- src/SolutionLeetcode.java
+++ src/SolutionLeetcode.java
@@ -10,7 +10,7 @@
  */
 public class SolutionLeetcode {
 	/**
-	 * problem 153. Find minimum in rotated sorted array.
+	 * Problem 153. Find minimum in rotated sorted array.
 	 * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 	 * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 	 * Find the minimum element.
@@ -18,7 +18,6 @@ public class SolutionLeetcode {
 	 * 
 	 * @param nums 
 	 * @return
-	 * @author ccding
 	 */
 	public int findMin(int[] nums){
 		int i = 0;
@@ -35,11 +34,75 @@ public class SolutionLeetcode {
 		}
 		return nums[i];	
 	}
+	/**
+	 * Problem 504. Base 7
+	 * Given an integer, return its base 7 string representation.
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public String convertToBase7(int num){
+		String s = "";
+		if(num == 0)
+			return "0";
+		boolean isNeg = num<0;
+		while(num!=0){
+			s = Math.abs(num)%7 + s;
+			num = num/7;
+		}
+		return isNeg ? "-"+s : s;
+	}
 	
+	public String convertToBase7_2(int num){
+		return Integer.toString(num, 7);
+	}
+	
+	/**
+	 * Problem 80. Remove duplicates from sorted array II
+	 * Follow up for "Remove Duplicates".
+	 * What if duplicates are allowed at most twice?
+	 * For example,
+	 * Given sorted array nums = [1,1,1,2,2,3],
+	 * Your function should return length = 5, with the first five 
+	 * elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what
+	 * you leave beyond the new length.
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int removeDuplicates(int[] nums){
+		if(nums.length <= 2)
+			return nums.length;
+		int i = 0, j = 0;			
+		int duplicate = 0;
+		while(++j != nums.length){
+			if(nums[j] == nums[i]){
+				duplicate++;
+				if(duplicate < 2){
+					nums[++i] = nums[j];
+				}
+			}else{
+				duplicate = 0;
+				nums[++i] = nums[j];
+			}
+		}
+		return i+1;
+	}
+	
+	public int removeDuplicates2(int[] nums){
+		int i = 0;
+		for(int n:nums){
+			if(i<2 || nums[i-2] < n){
+				nums[i++] = n;
+			}
+		}
+		return i;
+	}
 	
 	
 	public static void main(String[] args){
-		
+		SolutionLeetcode solution = new SolutionLeetcode();
+		System.out.println(solution.removeDuplicates2(new int[]{1,1,1,2,2,3}));
 	}
 	
 }
