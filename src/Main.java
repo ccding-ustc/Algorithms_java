ceec57d6093760810b6817f2e234078db9a663c1
ccding_ustc <andxdcc@sina.com>
2017-05-31 21:13:03 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
 	public static void main(String[] args){
 		SolutionLeetcode solution = new SolutionLeetcode();
-<<<<<<< HEAD
 //		ListNode node = new ListNode(-1);
 //		node.next = new ListNode(5);
 //		node.next.next = new ListNode(11);
@@ -14,15 +13,13 @@ public class Main {
 //		node2.next = new ListNode(10);
 //		ListNode[] lists = new ListNode[]{null, node, null, node2};
 //		int nums[] = new int[]{2,2,2,0,2,2};
-		System.out.println(solution.convert("PAYPALISHIRING", 3));
-=======
+//		System.out.println(solution.convert("PAYPALISHIRING", 3));
 		ListNode node = new ListNode(1);
 		node.next = new ListNode(5);
 		node.next.next = new ListNode(2);
 		node.next.next.next = new ListNode(3);
 		node.next.next.next.next = new ListNode(3);
-		System.out.println(solution.reverseWordsII( "the sky is blue"));
->>>>>>> b5e050895081876aa7aec02cb161c990fcbc5382
+//		System.out.println(solution.reverseWordsII( "the sky is blue"));
 		
 	}
 
