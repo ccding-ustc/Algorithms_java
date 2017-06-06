1385dafc166cc079217e71856c071fbdafafc336
ccding_ustc <andxdcc@sina.com>
2017-06-06 20:36:42 +0800

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,14 @@
+package main;
 import algorithms3th.StringMatcher;
 import utils.ListNode;
 
 public class Main {
 	public static void main(String[] args){
-		SolutionLeetcode solution = new SolutionLeetcode();
-//		System.out.println(solution.convert("PAYPALISHIRING", 3));
 		ListNode node = new ListNode(1);
 		node.next = new ListNode(5);
 		node.next.next = new ListNode(2);
 		node.next.next.next = new ListNode(3);
 		node.next.next.next.next = new ListNode(3);
-//		System.out.println(solution.reverseWordsII( "the sky is blue"));
 		System.out.println(StringMatcher.KMP("absghhsgdj", "hhs"));	
 	}
 }
